6404a42a01ff3b85a5827d304f36c6a186d2eaa5
farghalii <hamzafarghali25@gmail.com>
2024-12-19 23:12:21 +0200

    last

--- employee.sql
+++ employee.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2024 at 05:16 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Dec 19, 2024 at 10:11 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin123');
+(1, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -59,6 +59,39 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `employee_id`, `firstName`, `lastName`, `gender`, `phoneNum`, `position`, `image`, `date`) VALUES
+(1, 1, 'hamza', 'farghali', 'Male', 11021830, 'Web Developer (Front End)', 'E:\\\\4beff60c-783a-4645-b5f9-a64d0bd05838.jpg', '2024-12-19'),
+(2, 2, 'hamza', 'farghali', 'Male', 11021830, 'Web Developer (Front End)', 'E:\\\\4beff60c-783a-4645-b5f9-a64d0bd05838.jpg', '2024-12-19'),
+(3, 3, 'sameh', 'na7asjhguggjnk', 'Female', 3131387, 'App Developer', 'E:\\\\\\\\4beff60c-783a-4645-b5f9-a64d0bd05838.jpg', '2024-12-19'),
+(5, 4, 'sosssdadadadadaddad', 'fafa', 'Female', 313138720, 'Web Developer (Front End)', 'E:\\\\\\\\\\\\\\\\4beff60c-783a-4645-b5f9-a64d0bd05838.jpg', '2024-12-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_info`
+--
+
+CREATE TABLE `employee_info` (
+  `id` int(100) NOT NULL,
+  `employee_id` varchar(100) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `salary` double NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_info`
+--
+
+INSERT INTO `employee_info` (`id`, `employee_id`, `firstName`, `lastName`, `position`, `salary`, `date`) VALUES
+(1, '4', 'sosssdadadadadaddad', 'fafa', 'Web Developer (Front End)', 0, '2024-12-19');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -75,6 +108,12 @@ ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `employee_info`
+--
+ALTER TABLE `employee_info`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -88,7 +127,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `employee_info`
+--
+ALTER TABLE `employee_info`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
